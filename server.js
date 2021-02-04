71823e9283e6f44f15e130beab3f1302f35d7baf
vapoured99 <1704363@rgu.ac.uk>
2021-02-04 18:13:11 +0000

    adding routes

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ var port = PORT;
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
    });
+
+// create routes for the admin section
+//get an instance of the router
+var adminRouter = express.Router();
+// admin main page. the dashboard (http://localhost:PORT/admin)
+adminRouter.get('/', function(req, res) {
+ res.send('I am the dashboard!'); });
+// users page (http://localhost:PORT/admin/users)
+adminRouter.get('/users', function(req, res) {
+ res.send('I show all the users!'); });
+// posts page (http://localhost:PORT/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+ res.send('I show all the posts!'); });
+// apply the routes to our application
+app.use('/admin', adminRouter); 
+
    // start the server
    app.listen(PORT);
    console.log('Express Server running at http://127.0.0.1:'.PORT);
