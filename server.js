099cdc246c805cd9fa00fd6b33b1d5e32199944a
vapoured99 <1704363@rgu.ac.uk>
2021-02-04 18:32:20 +0000

    middleware

--- server.js
+++ server.js
@@ -8,22 +8,36 @@ var port = PORT;
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
    });
-   // start the server
-   app.listen(PORT);
-   console.log('Express Server running at http://127.0.0.1:'.PORT);
 
 
 // create routes for the admin section
 //get an instance of the router
 var adminRouter = express.Router();
+
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    console.log("Its middleware!!!");
+    // continue doing what we were doing and go to the route
+    next(); });
+
 // admin main page. the dashboard (http://localhost:PORT/admin)
-adminRouter.get('/', function(req, res) {
+adminRouter.use(function(req, res, next) {
+    //log each request to the console
  res.send('I am the dashboard!'); });
+
 // users page (http://localhost:PORT/admin/users)
 adminRouter.get('/users', function(req, res) {
  res.send('I show all the users!'); });
+
 // posts page (http://localhost:PORT/admin/posts)
 adminRouter.get('/posts', function(req, res) {
  res.send('I show all the posts!'); });
+ 
 // apply the routes to our application
 app.use('/admin', adminRouter); 
+
+   // start the server
+   app.listen(PORT);
+   console.log('Express Server running at http://127.0.0.1:'.PORT);
