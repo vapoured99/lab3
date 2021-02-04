188610c5685595639edb2a70bffcfee24df8f130
vapoured99 <1704363@rgu.ac.uk>
2021-02-04 17:42:15 +0000

    display html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require("http");
 
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 
@@ -7,14 +8,13 @@ http.createServer(function (request, response) {
     //send the http header
     //http status: 200 : OK
     //content type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, 
+        {'Content-Type': 'text/html',
+         'Access-Control-Allow-Origin' : '*'});
 
-    //send the response body as "Hello World"
-    response.end('Hello World\n');
-
-    
-    
-
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+    //send a message
+    readStream.pipe(respone);
 
 }).listen(PORT);
 
